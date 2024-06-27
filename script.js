99047bb57622a85d8e2c698cfd60bf5235797f3f
Rafa <126605152+Raf1d3@users.noreply.github.com>
2024-06-27 13:38:45 -0300

    adequações

--- script.js
+++ script.js
@@ -1,75 +1,20 @@
-const dropdown =  document.querySelector('.dropdown');
-const menu = document.querySelector('.dropdown-menu');
-const arrow = document.querySelector('.box-arrow-space')
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
 
-dropdown.onclick = () => {
-    menu.classList.toggle('ativo');
-    arrow.classList.toggle('ativo');
-}
-
-function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    let iconMobile = document.querySelector('.icon-arrow-mobile')
-    let iconWeb = document.querySelector('.icon-arrow')
-
-    if(menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        iconMobile.style.transform = 'rotate(223deg)';
-        iconWeb.style.transform = 'rotate(223deg)';
-        iconWeb.style.marginTop = '0px';
-
-    }
-    else {
-        menuMobile.classList.add('open');
-        iconMobile.style.transform = 'rotate(43deg)';
-        iconWeb.style.transform = 'rotate(43deg)';
-        iconWeb.style.marginTop = '5px';
-    }
-}
-
-function toggleMobileDropdown() {
-    let menuMobile = document.querySelector('.dropdown-menu-mobile');
-    let iconstyle = document.querySelector('.div-icon-arrow');
-
-    if(menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        iconstyle.style.transform = 'rotate(0deg)';
-    }
-    else {
-        menuMobile.classList.add('open');
-        iconstyle.style.transform = 'rotate(180deg)';
-    }
-}
-
-
-window.addEventListener('scroll', function(event) {
-    let imgDesktop = document.querySelector('.img-desktop');
-    let imgMobile = document.querySelector('.img-mobile');
-    let imgImgMobile = document.querySelector('.img-mobile img');
-    let imgImgDesktop = document.querySelector('.img-desktop img');
-    let scrollTop = document.documentElement.scrollTop;
-    
-    if (scrollTop === 0) {
-        imgMobile.classList.remove('imgStyle');
-        imgImgMobile.style.opacity = '0';
-        
-        imgDesktop.classList.remove('imgStyle');
-        imgImgDesktop.style.opacity = '0';
-        imgDesktop.style.paddingRight = '0px'
-      
-    } else {
-        imgMobile.classList.add('imgStyle');
-        imgImgMobile.style.opacity = '1';
-
-        imgDesktop.classList.add('imgStyle');
-        imgImgDesktop.style.opacity = '1'; 
-        imgDesktop.style.paddingRight = '60px'
-
-    }
-});
-
-
-
+ 
 // lading page
 //carrossel
 const carousel = document.querySelector(".carousel");
